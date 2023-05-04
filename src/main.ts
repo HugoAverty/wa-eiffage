923752d7555d6b90c89fc38622ef01eb5ff59c5b
Hugo Averty <hugoaverty@gmail.com>
2023-05-04 17:22:36 +0200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,28 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+
+    WA.room.onEnterLayer('_Settings/zoneBuilding').subscribe(() => {
+        WA.room.showLayer("Building/overlayBuilding");
+
+        WA.room.hideLayer("Building/aboveBuilding3");
+        WA.room.hideLayer("Building/aboveBuilding2");
+        WA.room.hideLayer("Building/aboveBuilding1");
+        WA.room.hideLayer("Building/Building2");
+        WA.room.hideLayer("Building/Building1");
+    })
+
+    WA.room.onLeaveLayer('_Settings/zoneBuilding').subscribe(() => {
+        WA.room.hideLayer("Building/overlayBuilding");
+        
+        WA.room.showLayer("Building/aboveBuilding3");
+        WA.room.showLayer("Building/aboveBuilding2");
+        WA.room.showLayer("Building/aboveBuilding1");
+        WA.room.showLayer("Building/Building2");
+        WA.room.showLayer("Building/Building1");
+    })
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
